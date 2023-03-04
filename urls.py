fa373b232bd97f8d7dc7f6529d7736a889c7b760
akhilvaka2 <118979080+akhilvaka2@users.noreply.github.com>
2023-03-04 09:23:06 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.employees_list, name='employees-list'),
-    path('create/', views.create_employee, name='create-employee'),
-    path('edit/<str:pk>/', views.edit_employee, name='edit-employee'),
-    path('delete/<str:pk>/', views.delete_employee, name='delete-employee'),
+    path('admin/', admin.site.urls),
+    path('', include('employee.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
